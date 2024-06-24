168086a491caeffa3d6d27fbf46c6ccd1b727aed
Zakhele Ndlovu <134063053+ZakheleNdlovu@users.noreply.github.com>
2024-06-24 11:23:56 +0100

    commit1

---
+++ Find_the_hypotenuse.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cmath>
+
+
+int main(){
+    double a;
+    double b;
+    double c;
+
+    std::cout << "Enter side a:";
+    std::cin>>a;
+
+    std::cout << "Enter side b:";
+    std::cin>>b;
+
+    c = sqrt(pow(a,2) + pow(b,2));
+
+    std::cout << "Side C is :" << c;
+
+    return 0;
+}
+
